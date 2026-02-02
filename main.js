6d4c4b656aab8c81533b25df4b703af6cf7b6573
t2life97 <t2life97@gmail.com>
2026-02-02 13:43:17 +0000

    feat: Create initial portfolio website

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-// Add JS here
+// Smooth scrolling for navigation links
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
