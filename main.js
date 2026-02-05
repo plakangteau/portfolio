15583adfd5a30a997e2f13581ad66624a6c0a3d7
t2life97 <t2life97@gmail.com>
2026-02-05 03:33:35 +0000

    feat: Add Disqus comment feature

--- main.js
+++ main.js
@@ -8,3 +8,43 @@ document.querySelectorAll('a.nav-link').forEach(anchor => {
         });
     });
 });
+
+var disqus_shortname = 'plakangteau';
+var disqus_loaded = false;
+
+function loadDisqus(identifier) {
+    var page_url = window.location.href.split('#')[0] + '#' + identifier;
+    var page_identifier = identifier;
+
+    if (window.DISQUS) {
+        DISQUS.reset({
+            reload: true,
+            config: function () {
+                this.page.url = page_url;
+                this.page.identifier = page_identifier;
+            }
+        });
+    } else {
+        var disqus_config = function () {
+            this.page.url = page_url;
+            this.page.identifier = page_identifier;
+        };
+
+        if (!disqus_loaded) {
+            var d = document, s = d.createElement('script');
+            s.src = 'https://' + disqus_shortname + '.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+            disqus_loaded = true;
+        }
+    }
+}
+
+var disqusModal = document.getElementById('disqus-modal');
+if (disqusModal) {
+    disqusModal.addEventListener('show.bs.modal', function (event) {
+        var button = event.relatedTarget;
+        var identifier = button.getAttribute('data-identifier');
+        loadDisqus(identifier);
+    });
+}
